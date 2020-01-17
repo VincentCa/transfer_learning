5c2431c9e5251409fcf3d3b3eec7f01b2fce8ae0
VincentCasser <vincent.casser@gmail.com>
2020-01-16 19:40:15 -0800

    helper

--- helpers.py
+++ helpers.py
@@ -16,9 +16,6 @@ import matplotlib.pyplot as plt
 from IPython.display import clear_output
 import cv2
 
-INPUT_SPATIAL = 224
-N_CLASSES = 6
-
 
 class CustomDataGenerator(tf.keras.utils.Sequence):
     """Custom data generator that yields tuples of (image, mask) for a pre-processed version of the Pascal VOC 2012 dataset."""
@@ -82,87 +79,8 @@ class CustomDataGenerator(tf.keras.utils.Sequence):
     
     def __len__(self):
         return int(np.floor(len(self.filenames) / self.batch_size))
-    
-class CustomCallback(tf.keras.callbacks.Callback):
-    """Custom callback to show a simple visualization of the losses during training."""
-    
-    def __init__(self, model, num_classes, smoothing=10, update_interval=20, gen_qualitative=None):
-        self.loss_train = []
-        self.loss_val = []
-        self.step = 0
-        self.epoch = 0
-        self.model = model
-        self.num_classes = num_classes
-        self.smoothing = smoothing
-        self.update_interval = update_interval
-        self.gen_qualitative = gen_qualitative
-        self.pred_qualitative = None
-        self.pred_qualitative_epoch = 0
-        self.on_epoch_end(0)
-        
-    def update_plots(self):
-        clear_output(wait=True)
-        print('Epoch', self.epoch)
-        
-        # Draw loss plot.
-        plt.figure(figsize=(10, 4))
-        plt_max = 0.0
-        valid = False
-        for index, pool in enumerate([self.loss_train, self.loss_val]):
-            moving_averages = []
-            for i in range(0, len(pool) - self.smoothing, self.smoothing):
-                values = pool[i:i+self.smoothing]
-                moving_averages.append(np.mean(values))
+   
 
-            valid = valid or len(moving_averages) > 0
-            if len(moving_averages) > 0:
-                plt.plot(range(len(moving_averages)), moving_averages, label='loss_' + ('val' if index else 'train'))
-                plt_max = max(plt_max, np.max(moving_averages))
-        
-        if valid:
-            plt.ylim([0, plt_max * 1.2])
-            plt.legend()
-            plt.show()
-
-        if self.pred_qualitative is not None:
-            num_pred = self.pred_qualitative.shape[0]
-            num_rows = num_pred // 5 + 1
-            
-            plt.figure(figsize=(15, 3 * num_rows))
-            plt.title('qualitative results, epoch ' + str(self.pred_qualitative_epoch))
-            for i in range(num_pred):
-                plt.subplot(num_rows, 5, i+1)
-                plt.imshow(self.pred_qualitative[i], vmin=0, vmax=self.num_classes, cmap='tab20')
-            plt.show()
-            
-    def on_train_begin(self, logs={}):
-        print('Epoch', self.epoch)
-        
-    def on_train_end(self, logs={}):
-        pass
-    
-    def on_epoch_end(self, epoch, logs={}):
-        self.epoch += 1
-        if self.gen_qualitative:
-            # Run qualitative generator and plot results. This should be a small generator!
-            pred = self.model.predict_generator(self.gen_qualitative)  # [N, H, W, N_CLASSES].
-            self.pred_qualitative = np.argmax(pred, axis=3)  # [N, H, W].  
-            self.pred_qualitative_epoch = self.epoch
-       
-    def on_batch_end(self, batch, logs=None):
-        current_loss = logs['loss']
-        if current_loss is not None:
-            self.loss_train.append(current_loss)
-            
-        if 'val_loss' in logs:  
-            current_loss_val = logs['val_loss']
-            if current_loss_val is not None:
-                self.loss_val.append(current_loss_val)
-
-        self.step += 1
-        if self.step % self.update_interval == 0:
-            self.update_plots()
-    
 def norm_vis(img, mode='rgb'):
     img_norm = (img - img.min()) / (img.max() - img.min())
     return img_norm if mode == 'rgb' else np.flip(img_norm, axis=2)
